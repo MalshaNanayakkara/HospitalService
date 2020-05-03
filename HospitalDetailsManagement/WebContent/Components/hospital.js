34c0bf8a75d99718374279b2253948fab8793f62
MalshaNanayakkara <malshananayakkara20@gmail.com>
2020-05-03 23:43:50 +0530

    Implement the update button

--- HospitalDetailsManagement/WebContent/Components/hospital.js
+++ HospitalDetailsManagement/WebContent/Components/hospital.js
@@ -28,3 +28,14 @@ $(document).on("click", "#btnSave", function(event)
 			// If valid------------------------
 			$("#formHospital").submit(); 
  });
+
+//UPDATE========================================== 
+$(document).on("click", ".btnUpdate", function(event) {    
+	$("#hidHospitalIDSave").val($(this).closest("tr").find('#hidHospitalIDUpdate').val());     
+	$("#hospitalName").val($(this).closest("tr").find('td:eq(0)').text());     
+	$("#hospitalProvince").val($(this).closest("tr").find('td:eq(1)').text());     
+	$("#hospitalDistrict").val($(this).closest("tr").find('td:eq(2)').text());     
+	$("#hospitalEmail").val($(this).closest("tr").find('td:eq(3)').text()); 
+	$("#hospitalPhone").val($(this).closest("tr").find('td:eq(4)').text()); 
+	$("#hospitalServices").val($(this).closest("tr").find('td:eq(5)').text()); 
+}); 
